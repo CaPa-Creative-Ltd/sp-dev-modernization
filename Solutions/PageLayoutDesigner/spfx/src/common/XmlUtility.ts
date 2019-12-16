78cf070d964c1b08ec7a3b84be70b502e3d65511
Paul Bullock <paul.bullock@capacreative.co.uk>
2019-12-16 16:41:56 +0000

    Added useful fragment to class

--- Solutions/PageLayoutDesigner/spfx/src/common/XmlUtility.ts
+++ Solutions/PageLayoutDesigner/spfx/src/common/XmlUtility.ts
@@ -3,3 +3,20 @@
   This class will be responsible for manipulating the xml file
 
 */
+
+/**
+ * Helper with general methods to simplify some routines
+ */
+export class XmlUtility {
+
+  /**
+   * Creates Document element based on Xml string
+   * @param xmlString XML string to parse
+   */
+  public static parseXml(xmlString): Document {
+    const parser = new DOMParser();
+    const xml = parser.parseFromString(xmlString, 'text/xml');
+    return xml;
+  }
+
+}
